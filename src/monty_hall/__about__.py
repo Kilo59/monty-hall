b9ed13952a46700d5f22ee47e59aa10ab92bb830
Gabriel Gore <gabriel59kg@gmail.com>
2023-05-27 10:57:16 -0400

    hatch project scaffold
    update project metadata

---
+++ src/monty_hall/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Gabriel Gore <gabriel59kg@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Gabriel Gore <gabriel59kg@gmail.com>
+#
+# SPDX-License-Identifier: MIT
